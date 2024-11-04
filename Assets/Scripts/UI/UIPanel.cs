5bcd6eca9b78cc41ce74dc21bd1d61c8f94ba96d
aquamarin102 <aquamarin120@gmail.com>
2024-11-04 13:45:40 +0300

    add buttons

--- Assets/Scripts/UI/UIPanel.cs
+++ Assets/Scripts/UI/UIPanel.cs
@@ -7,7 +7,9 @@ public class UIPanel : MonoBehaviour
 {
     [SerializeField] private GameObject _panelWin;
     [SerializeField] private GameObject _panelLose;
+    [SerializeField] private GameObject _soundIcon;
     private float _restartDelay = 0.5f;
+    private bool _soundIsOn = true;
 
     public void Win()
     {
@@ -22,6 +24,27 @@ public class UIPanel : MonoBehaviour
     public void OnButtonResetScene()
     {
         Invoke(nameof(ResetScene), _restartDelay);
+    }
+
+    public void OnButtonCloseGame() 
+    {
+        Application.Quit();
+    }
+
+    public void OnButtonOffSound()
+    {
+        if (!_soundIsOn)
+        {
+            _soundIcon.SetActive(true);
+            AudioListener.volume = 1.0f;
+            _soundIsOn = true;
+        }
+        else if (_soundIsOn)
+        {
+            _soundIcon.SetActive(false);
+            AudioListener.volume = 0f;
+            _soundIsOn = false;
+        }
     }
 
     public void ResetScene()
